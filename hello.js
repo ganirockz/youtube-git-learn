985d9289fe8cb6c3a35a8615717efdd00c730c8e
navya <navyasrirapeti1@gmail.com>
2022-07-09 12:40:37 +0530

    Modified hello.js file

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log('Welcome to Lets learn and code');
+let isSubscribed = false;
+if(isSubscribed){
+    console.log('Thanks for subscribing. Please share it to your friends');
+}else{
+    console.log('Please subscribe to Lets learn and code');
+}
